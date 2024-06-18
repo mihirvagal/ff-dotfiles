9a82b18a5173d8e2b90960a93806806df481e901
Mihir <77693447+mihirvagal@users.noreply.github.com>
2024-06-18 21:09:06 +0530

    fixed formatting

--- user-overrides.js
+++ user-overrides.js
@@ -11,23 +11,23 @@ user_pref("network.trr.uri", "https://dns.quad9.net/dns-query");
 user_pref("network.trr.custom_uri", "https://dns.quad9.net/dns-query");
 
 // enable disk cache
-user_pref("browser.cache.disk.enable", true)
+user_pref("browser.cache.disk.enable", true);
 
 // enable shortcuts' favicons
 // user_pref("browser.shell.shortcutFavicons", true)
 
 // enable HTTP background request
-user_pref("dom.security.https_only_mode_send_http_background_request", true)
+user_pref("dom.security.https_only_mode_send_http_background_request", true);
 
 // not set FF in private browsing mode
-user_pref("browser.privatebrowsing.autostart", false)
+user_pref("browser.privatebrowsing.autostart", false);
 
 // enable favicons in history and bookmarks
-user_pref("browser.chrome.site_icons", true)
+user_pref("browser.chrome.site_icons", true);
 
 // enable cache storage
-user_pref("browser.cache.dish.enable", true)
-user_pref("broser.sessionstore.privacy_level", 0)
+user_pref("browser.cache.dish.enable", true);
+user_pref("broser.sessionstore.privacy_level", 0);
 
 // enable session restore after reboot
-user_pref("toolkit.winRegisterApplicationRestart", true)
+user_pref("toolkit.winRegisterApplicationRestart", true);
